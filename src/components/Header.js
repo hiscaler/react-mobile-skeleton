3bcabef5a83270c066a233e5076f76dcf1802d5b
hiscaler <hiscaler@gmail.com>
2019-04-11 20:02:56 +0800

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+/**
+ * Header
+ *
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class Header extends Component {
+  
+  static defaultProps = {
+    text: "Mobile Skeleton",
+    className: "header"
+  }
+  
+  render() {
+    const {text, className} = this.props
+    console.info(this.props)
+    return (
+      <header className={className}>{text}</header>
+    );
+  }
+}
+
+export default Header;
