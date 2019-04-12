a2a167fb8a22c18815f9b6c657f9ac23690d9654
hiscaler <hiscaler@gmail.com>
2019-04-13 00:10:11 +0800

    完善路由

--- src/index.js
+++ src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import NewsList from "./components/homepage/NewsList";
 import NewsView from "./components/NewsView";
 import Error from "./components/Error";
+import NotFound from "./widgets/NotFound";
 
 ReactDOM.render((
   <Router>
-    <Route path="/" exact component={App}/>
-    <Route path="/news" exact component={NewsList}/>
-    <Route path="/news/:id" component={NewsView}/>
-    <Route path="/error" component={Error}/>
+    <Switch>
+      <Route path="/" exact component={App}/>
+      <Route path="/news" exact component={NewsList}/>
+      <Route path="/news/:id" component={NewsView}/>
+      <Route path="/error" component={Error}/>
+      <Route path="*" component={NotFound}/>
+    </Switch>
   </Router>
 ), document.getElementById('root'));
 
