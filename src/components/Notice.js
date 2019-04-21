779c7151b298ef057aca8043631831b1d8b954e6
hiscaler <hiscaler@gmail.com>
2019-04-21 15:29:04 +0800

    Add Notice component

---
+++ src/components/Notice.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import {NoticeBar} from "antd-mobile"
+import Url from "../helpers/Url"
+import axios from "axios"
+
+/**
+ * Notice component
+ *
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class Notice extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      item: []
+    }
+  }
+  
+  componentWillMount() {
+    const url = Url.toRoute('news/list', {
+      limit: this.props.limit
+    })
+    axios.get(url).then((resp) => {
+      if (resp.data.success) {
+        this.setState({
+          item: resp.data.data.items[0]
+        })
+      }
+    })
+  }
+  
+  render() {
+    const {item} = this.state
+    if (item) {
+      return (
+        <NoticeBar marqueeProps={{loop: false, style: {padding: '0 7.5px'}}}>
+          {item.title}
+        </NoticeBar>
+      );
+    } else {
+      return null
+    }
+    
+  }
+  
+}
+
+export default Notice
