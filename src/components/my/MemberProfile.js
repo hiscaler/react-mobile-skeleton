f5d810c16e7e70320204a082791258a577446de3
hiscaler <hiscaler@gmail.com>
2019-05-04 21:52:55 +0800

    完善会员资料更新处理

--- src/components/my/MemberProfile.js
+++ src/components/my/MemberProfile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Button, ImagePicker, InputItem, List, TextareaItem, Toast, WhiteSpace, WingBlank} from "antd-mobile"
+import {Button, ImagePicker, InputItem, List, Modal, TextareaItem, Toast, WhiteSpace, WingBlank} from "antd-mobile"
 import axios from 'axios'
 import Url from "../../helpers/Url"
 import Item from "antd-mobile/es/popover/Item"
@@ -19,7 +19,9 @@ class MemberProfile extends React.Component {
       files: [],
       real_name: undefined,
       mobile_phone: undefined,
-      remark: undefined
+      remark: undefined,
+      showModal: false,
+      errors: undefined,
     }
     
     this.onChange = this.onChange.bind(this)
@@ -28,11 +30,22 @@ class MemberProfile extends React.Component {
   
   componentWillMount() {
     this.setState({
-      username: "hiscaler",
-      avatar: null,
-      real_name: "数字",
-      mobile_phone: 15888888888,
-      remark: ''
+      errors: [],
+    })
+  }
+  
+  componentDidMount() {
+    const url = Url.toRoute('member/view')
+    axios.get(url).then(res => {
+      const member = res.data.data
+      console.info(member)
+      this.setState({
+        username: member.username,
+        files: [{url: member.avatar}],
+        real_name: member.real_name,
+        mobile_phone: member.mobile_phone,
+        remark: member.remark,
+      })
     })
   }
   
@@ -59,10 +72,15 @@ class MemberProfile extends React.Component {
       if (error.response) {
         let errorMessages = []
         const messages = error.response.data.error
+        
         for (let i in messages) {
-          errorMessages.push((parseInt(i) + 1) + '. ' + messages[i].message)
+          errorMessages.push(messages[i].message)
         }
-        Toast.fail(errorMessages)
+        console.info(errorMessages)
+        this.setState({
+          showModal: true,
+          errors: errorMessages
+        })
       }
     })
     event.preventDefault()
@@ -72,65 +90,87 @@ class MemberProfile extends React.Component {
     const {username, files, real_name, mobile_phone, remark} = this.state
     return (
       <WingBlank>
-        <form>
-          <List>
-            <div className="avatar">
-              <ImagePicker
-                length="6"
-                files={files}
-                onChange={this.onChange}
-                onImageClick={(index, fs) => console.log(index, fs)}
-                selectable={files.length < 1}
-                length={1}
-                disableDelete
-                accept="image/gif,image/jpeg,image/jpg,image/png"
+        <Modal
+          visible={this.state.showModal}
+          transparent
+          maskClosable={false}
+          title="提示信息"
+          footer={[{
+            text: '关闭', onPress: () => {
+              console.log('ok');
+              this.setState({showModal: false});
+            }
+          }]}
+          wrapProps={{onTouchStart: this.onWrapTouchStart}}
+        >
+          <div class="errors">
+            <ul>
+              {this.state.errors.map((msg, index) => <li>{index + 1}. {msg}</li>)}
+            </ul>
+          </div>
+        </Modal>
+        {
+          username &&
+          <form>
+            <List>
+              <div className="avatar">
+                <ImagePicker
+                  length="6"
+                  files={files}
+                  onChange={this.onChange}
+                  onImageClick={(index, fs) => console.log(index, fs)}
+                  selectable={files.length < 1}
+                  length={1}
+                  disableDelete
+                  accept="image/gif,image/jpeg,image/jpg,image/png"
+                />
+              </div>
+              <InputItem
+                type="text"
+                defaultValue={username}
+                editable={false}
+              >账号</InputItem>
+              <InputItem
+                type="text"
+                name="real_name"
+                defaultValue={real_name}
+                value={real_name}
+                placeholder="请输入您的姓名"
+                clear
+                moneyKeyboardAlign="left"
+                onChange={(real_name) => {
+                  this.setState({real_name})
+                }}
+              >姓名</InputItem>
+              <InputItem
+                type="phone"
+                name="mobile_phone"
+                defaultValue={mobile_phone}
+                placeholder="请输入您的手机号码"
+                clear
+                moneyKeyboardAlign="left"
+                onChange={(mobile_phone) => {
+                  this.setState({mobile_phone})
+                }}
+              >手机号码</InputItem>
+              <TextareaItem
+                title="备注"
+                name="remark"
+                defaultValue={remark}
+                placeholder="请输入备注"
+                ref={el => this.autoFocusInst = el}
+                autoHeight
+                onChange={(remark) => {
+                  this.setState({remark})
+                }}
               />
-            </div>
-            <InputItem
-              type="text"
-              defaultValue={username}
-              editable={false}
-            >账号</InputItem>
-            <InputItem
-              type="text"
-              name="real_name"
-              defaultValue={real_name}
-              value={real_name}
-              placeholder="请输入您的姓名"
-              clear
-              moneyKeyboardAlign="left"
-              onChange={(real_name) => {
-                this.setState({real_name})
-              }}
-            >姓名</InputItem>
-            <InputItem
-              type="phone"
-              name="mobile_phone"
-              defaultValue={mobile_phone}
-              placeholder="请输入您的手机号码"
-              clear
-              moneyKeyboardAlign="left"
-              onChange={(mobile_phone) => {
-                this.setState({mobile_phone})
-              }}
-            >手机号码</InputItem>
-            <TextareaItem
-              title="备注"
-              name="remark"
-              defaultValue={remark}
-              placeholder="请输入备注"
-              ref={el => this.autoFocusInst = el}
-              autoHeight
-              onChange={(remark) => {
-                this.setState({remark})
-              }}
-            />
-            <WhiteSpace/>
-            <Item>
-              <Button type="primary" onClick={this.handleSubmit}>保存</Button>
-            </Item>
-          </List>
-        </form>
+              <WhiteSpace/>
+              <Item>
+                <Button type="primary" onClick={this.handleSubmit}>保存</Button>
+              </Item>
+            </List>
+          </form>
+        }
       </WingBlank>
     )
   }
