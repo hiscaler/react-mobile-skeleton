15dab9033d38c861afbc7ffd06229d8a224517c8
hiscaler <hiscaler@gmail.com>
2019-05-06 21:11:00 +0800

    重构 Url.toRoute 函数

--- src/helpers/Url.js
+++ src/helpers/Url.js
@@ -12,24 +12,37 @@ class Url {
    * 生成 API 访问地址
    *
    * @param router
-   * @param queries
+   * @param params
    * @returns {string}
    */
-  static toRoute(router, queries = {}) {
+  static toRoute(router, params = {}) {
     let qs = []
-    for (let q in queries) {
-      qs.push(q + "=" + queries[q])
+    for (let q in params) {
+      qs.push(q + "=" + params[q])
     }
-    let url = Config.url + '/' + router + '?'
+    let url = Config.url.trim()
+    if (url[url.length] !== '/') {
+      url += '/'
+    }
+    router = router.trim()
+    if (router.length) {
+      router = router.replace('\\', '//')
+      if (router.startsWith('/')) {
+        router = router.substring(1)
+      }
+      if (router.endsWith('/')) {
+        router = router.substring(0, router.length - 1)
+      }
+      url += router
+    }
+    url += url.indexOf('?') === -1 ? '?' : '&'
     if (qs.length) {
-      url += qs.join('&') + '&'
+      url += qs.join('&')
     }
     const identity = User.getIdentity();
     if (identity && !identity._isGuest && identity._accessToken) {
-      url += 'accessToken=' + identity._accessToken;
+      url += '&accessToken=' + identity._accessToken;
     }
-    
-    console.info("API URL is " + url)
     
     return url
   }
